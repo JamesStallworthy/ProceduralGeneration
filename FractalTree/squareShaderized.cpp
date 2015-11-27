52033316337c1428ead09dc109d7f0742264b964
James Stallworthy <stallwo2@uni.coventry.ac.uk>
2015-11-27 15:49:18 +0000

    Camera Controls

--- FractalTree/squareShaderized.cpp
+++ FractalTree/squareShaderized.cpp
@@ -55,9 +55,16 @@ static enum object {SQUARE};
 TreeBufferPos pos;
 Vertex drawVertices[2000];
 
-static Matrix4x4
-modelViewMat = IDENTITY_MATRIX4x4;
+mat4 modelViewMat = mat4(1.0);
 static mat4 projMat = mat4(1.0);
+
+//Camera
+const float SPEED = 1;
+const float ROTSPEED = 2;
+float cameraTheta = 0;
+vec3 cameraPos(0, 0, 100);
+//Line of sight
+vec3 LOS(0, 0, -1);
 
 static unsigned int
    programId,
@@ -127,31 +134,39 @@ void setup(void)
 
    // Obtain projection matrix uniform location and set value.
    projMatLoc = glGetUniformLocation(programId, "projMat");
-   projMat = glm::ortho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+   //projMat = glm::ortho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+   projMat = glm::perspective(1.0472, 1.0, 0.1, 200.0);
    glUniformMatrix4fv(projMatLoc, 1, GL_FALSE, value_ptr(projMat));
    ///////////////////////////////////////
 
    // Obtain modelview matrix uniform location and set value.
-   Matrix4x4 modelViewMat = IDENTITY_MATRIX4x4;
+   //Matrix4x4 modelViewMat = IDENTITY_MATRIX4x4;
+   modelViewMat = lookAt(vec3(0, 0, 100), vec3(0, -15, 0), vec3(0,1,0));
    modelViewMatLoc = glGetUniformLocation(programId, "modelViewMat"); 
-   glUniformMatrix4fv(modelViewMatLoc, 1, GL_TRUE, modelViewMat.entries);
+   glUniformMatrix4fv(modelViewMatLoc, 1, GL_FALSE, value_ptr(modelViewMat));
    ///////////////////////////////////////
 }
 
 // Drawing routine.
 void drawScene(void)
 {
-   glClear(GL_COLOR_BUFFER_BIT);
-   //Draw tree
-   int q = 0;
-   for (int i = pos.treeStart; i < pos.treeFinish +1; i += 2) {
-	   glLineWidth( pos.depth[(q-1)/2]);
-	   glDrawArrays(GL_LINES, i, 2);
-	   q += 2;
+	LOS.x = sin(cameraTheta*3.14/180);
+	LOS.z = -cos(cameraTheta*3.14/180);
+	LOS = glm::normalize(LOS);
+	modelViewMat = lookAt(vec3(cameraPos.x, cameraPos.y, cameraPos.z), vec3(cameraPos.x+LOS.x, cameraPos.y+LOS.y, cameraPos.z + LOS.z), vec3(0, 1, 0));
+	glUniformMatrix4fv(modelViewMatLoc, 1, GL_FALSE, value_ptr(modelViewMat));
+    glClear(GL_COLOR_BUFFER_BIT);
+    //Draw tree
+	//glDrawArrays(GL_LINES, 0, pos.treeFinish+1);
+    int q = 0;
+    for (int i = pos.treeStart; i < pos.treeFinish +1; i += 2) {
+	    glLineWidth( pos.depth[(q-1)/2]);
+	    glDrawArrays(GL_LINES, i, 2);
+	    q += 2;
    }
    //Draw leaves
    for (int i = pos.leafStart; i < pos.leafFinish; i += 4) {
-	   glDrawArrays(GL_TRIANGLE_STRIP, i, 4);
+	    glDrawArrays(GL_TRIANGLE_STRIP, i, 4);
    }
   
    glFlush();
@@ -162,17 +177,45 @@ void resize(int w, int h)
 {
    glViewport(0, 0, w, h);
 }
+void reGen() {
+	tree Tree;
+	pos = Tree.genTree(drawVertices, 0, 8, 25, 25);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[SQUARE_VERTICES]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(drawVertices), drawVertices, GL_STATIC_DRAW);
+}
 
 // Keyboard input processing routine.
 void keyInput(unsigned char key, int x, int y)
 {
-   switch(key) 
-   {
-      case 27:
-         exit(0);
-         break;
-      default:
-         break;
+	switch(key) 
+	{
+		case 27:
+			exit(0);
+			break;
+		case 119://Forward
+			cameraPos.x += LOS.x * SPEED;
+			cameraPos.z += LOS.z * SPEED;
+			glutPostRedisplay();
+			break;
+		case 115:
+			cameraPos.x -= LOS.x * SPEED;
+			cameraPos.z -= LOS.z * SPEED;
+			glutPostRedisplay();
+			break;
+		case 97:
+			cameraTheta-=ROTSPEED;
+			glutPostRedisplay();
+			break;
+		case 100:
+			cameraTheta+=ROTSPEED;
+			glutPostRedisplay();
+			break;
+		case 114:
+			reGen();
+			glutPostRedisplay();
+			break;
+		default:
+			break;
    }
 }
 
